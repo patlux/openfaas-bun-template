e97353ed31f02f27b4b90520df7f071721176f07
Patrick Wozniak <email@patwoz.de>
2023-06-11 09:00:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# openfaas-bun-template
+
+Ready-to-use template for bootstraping a bun function for openfaas!
+
+## Usage
+
+```sh
+faas-cli template pull https://github.com/patlux/openfaas-bun-template
+faas-cli new my-fancy-bun --lang bun
+```
+
---
+++ template/bun/function/index.ts
@@ -0,0 +1,24 @@
+import type { Server, Serve } from 'bun'
+
+console.log('Hello via Bun!')
+
+export const bunServeOptions: Serve = {
+  port: 3000,
+  fetch: (request: Request) => {
+    const url = new URL(request.url)
+    console.log(`>> [${request.method}] ${url.pathname}`)
+    return new Response('Hello via Bun!', { status: 200 })
+  },
+}
+
+// This enables the hot reload mode with `bun --hot`
+
+declare global {
+  var server: Server
+}
+
+if (!globalThis.server) {
+  globalThis.server = Bun.serve(bunServeOptions)
+} else {
+  globalThis.server.reload(bunServeOptions)
+}
